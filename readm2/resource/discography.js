93c5deb984bf2c920377cc600ebcbbd356be0f22
readm2 <readm2@readm2ui-MacBook-Pro.local>
2018-05-21 02:13:30 +0900

    Change to dynamic load

---
+++ readm2/resource/discography.js
@@ -0,0 +1,106 @@
+var discographies = [
+    {
+        type: "DJ",
+        title: "FirstKiss Party",
+        date: "NOV 20, 2015",
+        place: "@ SUWON TINPAN CLUB",
+        imageurl: "resource/poster_151120.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "Pre LuckySeven Party",
+        date: "MAR 18, 2016",
+        place: "@ SUWON TINPAN CLUB",
+        imageurl: "resource/poster_160318.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "LuckySeven Party",
+        date: "MAR 25, 2016",
+        place: "@ ITAEWON DIPTIQUE CLUB",
+        imageurl: "resource/poster_160318.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "Samsung 55th BlueSketch",
+        date: "JUN 17, 2016",
+        place: "@ SAMSUNG ELECTRONICS",
+        imageurl: "",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "Hot Summer Special Party",
+        date: "JUL 22, 2016",
+        place: "@ ITAEWON DIPTIQUE CLUB",
+        imageurl: "resource/poster_160722.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "BBQ Music All Day Party",
+        date: "OCT 22, 2016",
+        place: "@ ITAEWON JR SOUTHERN BBQ",
+        imageurl: "resource/poster_161022.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "Music of The Spotlight",
+        date: "NOV 11, 2016",
+        place: "@ SUWON WS LOUNGE CLUB",
+        imageurl: "resource/poster_161111.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "2016 YEAR END PARTY",
+        date: "DEC 16, 2016",
+        place: "@ SUWON JUKEBOX",
+        imageurl: "resource/poster_161216.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "SPOTLIGHT PARTY",
+        date: "MAR 10, 2017",
+        place: "@ SUWON ARCHEPUB",
+        imageurl: "",
+        linkurl: "https://news.samsung.com/kr/%ED%9D%A5%E8%88%88-%EB%A7%8E%EC%9D%80-%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90-%EC%9E%84%EC%A7%81%EC%9B%90-%EC%97%AC%EA%B8%B0-%EB%8B%A4-%EB%AA%A8%EC%98%80%EB%84%A4"
+    },
+    {
+        type: "DJ",
+        title: "NINE ONE SIX PARTY",
+        date: "SEPT 16, 2017",
+        place: "@ ITAEWON TEMPLE LOUNGE",
+        imageurl: "resource/poster_170916.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "DRAGON PARTY",
+        date: "OCT 21, 2017",
+        place: "@ ITAEWON CHUNHYANG CLUB",
+        imageurl: "",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "WHIMORY PARTY",
+        date: "MAR 24, 2018",
+        place: "@ ITAEWON TEMPLE LOUNGE",
+        imageurl: "resource/poster_180324.png",
+        linkurl: ""
+    },
+    {
+        type: "DJ",
+        title: "Techtree",
+        date: "MAY 05, 2018",
+        place: "@ ITAEWON G-15 Sonnendeck",
+        imageurl: "resource/poster_180505.jpg",
+        linkurl: ""
+    }
+];
